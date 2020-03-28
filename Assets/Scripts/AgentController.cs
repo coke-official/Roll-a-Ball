5a2be14947dcf59df7aa9f5f15664c5e71873e48
coke-official <cokeofficial1886@gmail.com>
2020-03-28 10:27:10 +0900

    first commit

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AgentController : MonoBehaviour
+{
+   public float agent_speed = 10;
+    public float agent_jump = 1;
+    private bool jumping = true;
+    public bool restart = false;
+    private Vector3 zero = new Vector3 (0.0f, 0.0f, 0.0f);
+
+   void FixedUpdate ()
+   {
+       float x = Input.GetAxis("Horizontal");
+       float z = Input.GetAxis("Vertical");
+       Rigidbody rb = GetComponent<Rigidbody>();
+
+       
+       if (jumping == false && Input.GetKey(KeyCode.Space)) {
+           rb.AddForce(0, agent_jump, 0, ForceMode.Impulse);
+           jumping = true;
+       }else
+       {
+           rb.AddForce(x*agent_speed, 0, z*agent_speed);
+       }
+       if (rb.position.y < -30){
+           restart = true;
+       }
+        if (restart){
+            rb.position = zero + Vector3.up*2;
+            rb.angularVelocity = zero;
+            restart = false;
+            jumping = true;
+        }
+   }
+
+   void OnCollisionEnter(Collision hit)
+   {
+       jumping = false;
+
+       if (hit.gameObject.tag == "Traps")
+       {
+           restart = true;
+       }  
+       if (hit.gameObject.tag == "Food")     
+       {
+           Destroy(hit.gameObject);
+       }
+   }  
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject agent;
+    public GameObject food;
+    public GameObject cleraLabelObject;
+    public UnityEngine.UI.Text dis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cleraLabelObject.SetActive (false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (food != null){
+            Vector3 Agentpos = agent.transform.position;
+            Vector3 Foodpos = food.transform.position;
+            float dist = Vector3.Distance(Agentpos, Foodpos);
+            dis.text = "Distance : " + dist;
+        }else{
+            cleraLabelObject.SetActive (true);
+        }
+        
+    }
+}
